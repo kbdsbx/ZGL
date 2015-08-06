ec62d1b3deedbbf52ae9284f54fa02c5972020bb
Zhao Yi <z@zhaoyi.me>
2015-08-06 18:04:39 +0800

    Gridding

--- ZGL/geometry/gridding.h
+++ ZGL/geometry/gridding.h
@@ -8,21 +8,24 @@
 #define ZGL_GRIDDING
 
 namespace ZGL {
-	// Gridding
-	// 网格
+	/// Gridding
+	/// 网格
 	template < z_size_t dim, z_size_t d_dim, typename Titem >
 	class gridding
 		: public Iseparable {
+	protected :
 		typedef Titem _Titem;
-		typedef gridding< dim, d_dim, _Titem > _Tself;
+		typedef gridding < dim, d_dim, _Titem > _Tself;
 		typedef affine_vector < dim, _Titem > _Tv;
+		typedef vector < dim, z_size_t, void > _Tidx;
+
 	public :
 
 		// Discrete data
 		// 离散数据
 		class grid_data {
 			_Titem* data = nullptr;
-			std::function< void(_Titem[dim - 1]) > f = nullptr;
+			std::function< void(const _Tv&) > f = nullptr;
 
 		public :
 			~grid_data() {
@@ -38,7 +41,7 @@ namespace ZGL {
 				for (z_size_t i = 0; i < ct; i++)
 					data[i] = begin += step;
 			}
-			// Add discrete date at array
+			// Add discrete data at array
 			// 数组添加数据
 			grid_data(_Titem* src, z_size_t len) {
 				if (len > 0)
@@ -48,10 +51,35 @@ namespace ZGL {
 			}
 			// Make discrete date use function
 			// 使用方法生成离散数据
-			grid_data(const std::function< void(_Titem*, z_size_t) >& func) {
+			grid_data(const std::function< void(const _Tv&) >& func) {
 				f = func;
 			}
-		} _grid_data[dim - 1];
+
+			// whether discrete data
+			// 是否是离散数据
+			bool is_data() const {
+				return (data != nullptr);
+			}
+
+			// whether function
+			// 是否是方法
+			bool is_function() const {
+				return (f != nullptr);
+			}
+
+			// Get the I-th data
+			// 获取第I个数据
+			_Titem operator[] (z_size_t idx) const {
+				return data[idx];
+			}
+
+			// Generate the I-th data
+			// 生成第I个数据
+			_Titem operator[] (const _Tv& idx) const {
+				return f(idx);
+			}
+		};
+		grid_data _grid_data[dim - 1];
 
 		class grid_node {
 			grid_node** nodes;
@@ -59,14 +87,16 @@ namespace ZGL {
 		public :
 			~grid_node() {
 				z_size_t d_size = (z_size_t)pow(2, d_dim);
-				for (z_size_t i = 0; i < d_size; i++)
+				for (z_size_t i = 0; i < d_size; i++) {
 					if (nodes[i])
 						delete nodes[i];
+					nodes[i] = nullptr;
+				}
 				delete[] nodes;
 			}
 
 			grid_node() {
-				nodes = new grid_node*[(z_size_t)pow(2, d_dim)];
+				nodes = new grid_node*[(z_size_t)pow(2, d_dim)]{ nullptr };
 			}
 
 			grid_node(const _Tv& dot)
@@ -77,6 +107,18 @@ namespace ZGL {
 
 			grid_node* operator [] (z_size_t opt) {
 				return nodes[opt];
+			}
+
+			// Set dot
+			// 设置点
+			void set_dot(const _Tv& dot) {
+				_dot = dot;
+			}
+
+			// Set dot
+			// 设置点
+			void set_dot(_Tv&& dot) {
+				_dot = dot;
 			}
 		}
 
@@ -90,14 +132,46 @@ namespace ZGL {
 				_grid_data[i] = data[i];
 		}
 
-		grid_node& discrete() {
+		// Generate discrete dots' map
+		// 生成离散图
+		grid_node discrete() {
 			grid_node _root;
 
-			_Tv _t;
-			for (z_size_t i = 0; i < dim - 1; i++) {
-			}
+			_Tidx idx;
+
+			_dis(_root, idx);
 
 			return std::move(_root);
+		}
+
+		/// Recursion nodes within map
+		/// 递归图节点
+		///
+		/// node : current node
+		///	       当前节点
+		/// idx  : current index of node
+		///        当前节点索引
+		void _dis(grid_node& node, const _Tidx& idx) {
+			_Tv _dt;
+			for (z_size_t i = 0; i < dim - 1; i++) {
+				if (_grid_data[i].is_data())
+					_dt[i] = _grid_data[i][idx[i]];
+				else if (_grid_data[i].is_function())
+					_dt[i] = _grid_data[i][_dt];
+				else
+					throw "";
+			}
+			_dt[dim - 1] = _Titem(1);
+			node.set_dot(_dt);
+
+			for (z_size_t i = 0; i < dim - 1; i++) {
+				if (!node[i]) {
+					node[i] = new grid_node();
+					_Tidx _idx(idx);
+					idx[i]++;
+					_dis(node[i], idx);
+				}
+			}
 		}
 	};
 }
